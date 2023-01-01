2cc13c3146b0331d7e6dae22be805b0bc3335567
farbodlk <farbodm2004@gmail.com>
2023-01-01 21:53:31 +0330

    https://github.com/farbodlk/web.git

---
+++ script.js
@@ -0,0 +1,65 @@
+const productContainers = [...document.querySelectorAll(".product-container")];
+const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
+const preBtn = [...document.querySelectorAll(".pre-btn")];
+
+productContainers.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtn[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth;
+  });
+
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
+
+const menu_btn = document.querySelector(".berger");
+const mobile_menu = document.querySelector(".nav-animation ");
+
+menu_btn.addEventListener("click", function () {
+  mobile_menu.classList.toggle("active");
+  mobile_menu.classList.remove(".navbar-collapse");
+});
+
+function findJob() {
+  const errorJob = document.querySelector(".error-job");
+  const errorCat = document.querySelector(".error-cat");
+  const errorMsg = document.querySelector(".error-msg");
+
+errorMsg.classList.remove("show-error-msg");
+errorCat.classList.remove("show-error-msg");
+errorJob.classList.remove("show-error-msg"); 
+  var loc = document.getElementById("Location").value;
+  var cat = document.getElementById("All_Category").value;
+  var job = document.getElementById("Job_Title").value;
+
+  if (loc === "") {
+    errorMsg.classList.add("show-error-msg");
+    setTimeout(function () {  }, 1000);
+  }
+
+  if  (cat === "") {
+    errorCat.classList.add("show-error-msg");
+    setTimeout(function () { }, 1000);
+  }
+
+  if (job === "") {
+    errorJob.classList.add("show-error-msg");
+    setTimeout(function () {  }, 1000);
+  } 
+
+   else {
+     
+  if(job && loc && cat) {
+      window.location.href =
+        "https://www.google.com/search?q=i+am+a+" +
+        job +
+        "+looking+for+a+" +
+        cat +
+        "+position+in+" +
+        loc;
+    }
+  }
+}
