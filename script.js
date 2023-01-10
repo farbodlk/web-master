c92b1f59ca0a2bbf19221bd70eec79fe67e55410
farbodlk <farbodm2004@gmail.com>
2023-01-10 10:15:54 +0330

    https://github.com/farbodlk/web-master.git

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const productContainers = [...document.querySelectorAll(".product-container")];
 const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
 const preBtn = [...document.querySelectorAll(".pre-btn")];
-
+var myModal = new bootstrap.Modal(document.getElementById("exampleModal"));
+const menu_btn = document.querySelector(".berger");
+const mobile_menu = document.querySelector(".nav-animation ");
+const search = {};
+const errorJob = document.querySelector(".error-job");
+const errorCat = document.querySelector(".error-cat");
+const errorMsg = document.querySelector(".error-msg");
 productContainers.forEach((item, i) => {
   let containerDimensions = item.getBoundingClientRect();
   let containerWidth = containerDimensions.width;
 
   nxtBtn[i].addEventListener("click", () => {
     item.scrollLeft += containerWidth;
-    console.log( item.scrollLeft);
-    if ( item.scrollLeft===0 ) {
-      
+    console.log(item.scrollLeft);
+    if (item.scrollLeft === 0) {
     }
   });
 
@@ -19,61 +24,62 @@ productContainers.forEach((item, i) => {
   });
 });
 
-const menu_btn = document.querySelector(".berger");
-const mobile_menu = document.querySelector(".nav-animation ");
-
 menu_btn.addEventListener("click", function () {
   mobile_menu.classList.toggle("active");
   mobile_menu.classList.remove(".navbar-collapse");
 });
 
-var myModal = new bootstrap.Modal(document.getElementById('exampleModal'))
-startCountdown ()
-
 function findJob() {
-  const errorJob = document.querySelector(".error-job");
-  const errorCat = document.querySelector(".error-cat");
-  const errorMsg = document.querySelector(".error-msg");
-errorMsg.classList.remove("show-error-msg");
-errorCat.classList.remove("show-error-msg");
-errorJob.classList.remove("show-error-msg"); 
+  errorMsg.classList.remove("show-error-msg");
+  errorCat.classList.remove("show-error-msg");
+  errorJob.classList.remove("show-error-msg");
   var loc = document.getElementById("Location").value;
   var cat = document.getElementById("All_Category").value;
   var job = document.getElementById("Job_Title").value;
 
   if (loc === "") {
     errorMsg.classList.add("show-error-msg");
-    setTimeout(function () {  }, 1000);
-
+    setTimeout(function () {}, 1000);
   }
 
-  if  (cat === "") {
+  if (cat === "") {
     errorCat.classList.add("show-error-msg");
-    setTimeout(function () { }, 1000);
+    setTimeout(function () {}, 1000);
   }
 
   if (job === "") {
     errorJob.classList.add("show-error-msg");
-    setTimeout(function () {  }, 1000);
-  } 
-
-
-
-   else {
-     
-  if(job && loc && cat) {
-   
-      myModal.show()
- timeRre()
-    //    
-    //     "https://www.google.com/search?q=i+am+a+" +
-    //      job +
-    //     "+looking+for+a+" +
-    //     cat +
-    //     "+position+in+" +
-    //     loc;   window.location.href =
-  
+    setTimeout(function () {}, 1000);
+  } else {
+    if (job && loc && cat) {
+      myModal.show();
+      delayRedirect(job, cat, loc);
     }
   }
 }
 
+function delayRedirect(j, c, l) {
+  var seconds = 5;
+
+  var lblCount = document.getElementById("lblCount");
+
+  lblCount.innerHTML = seconds;
+
+  page = setInterval(function () {
+    seconds--;
+    lblCount.innerHTML = seconds;
+    if (seconds == 0) {
+      window.location.href =
+        "https://www.google.com/search?q=i+am+a+" +
+        j +
+        "+looking+for+a+" +
+        c +
+        "+position+in+" +
+        l;
+    }
+  }, 1000);
+}
+const myModalEl = document.getElementById("exampleModal");
+myModalEl.addEventListener("hidden.bs.modal", (event) => {
+  clearInterval(page);
+});
